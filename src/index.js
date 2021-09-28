6b3e65edddb08a426a52ffc322e7706e20f029be
Emre Kırcı <emre.kirci@dormakaba.com>
2021-09-28 15:34:28 +0200

    added practice project without style

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import App2 from "./App2";
+import App3 from "./App3";
 
-ReactDOM.render(<App2 />, document.getElementById("root"));
+ReactDOM.render(<App3 />, document.getElementById("root"));
